a4a2810fc9ea85549c84b6140e10c8d62249ec79
Cristin Jenkins <cristindjenkins@gmail.com>
2023-05-31 04:24:41 +0000

    reassigning-values

---
+++ reassigning-values.py
@@ -0,0 +1,25 @@
+age=28
+print(age)
+
+age = 28
+new_age = age + 5
+print(new_age)
+
+age = 28
+age = age + 7
+print (age)
+
+age = 28
+age += 7
+print (age)
+
+age = 28
+age -= 7
+print (age)
+
+text = 'hokus' + 'pokus'
+print (text)
+
+text = 'hokus' * 5
+print (text)
+
