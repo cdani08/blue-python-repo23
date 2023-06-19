d9fd423870b9c9160fcdca2f07e7c50318dedda2
Cristin Jenkins <cristindjenkins@gmail.com>
2023-06-19 13:49:57 +0000

    modified code

--- wk_13_project.py
+++ wk_13_project.py
@@ -1,25 +1,12 @@
 import os
 
-cwd = os.getcwd()
-print("Current working directory: {0}".format(cwd))
-
 current_directory = os.getcwd()
-
 files = os.listdir()
-
 files_dict = {}
 
-
 for file in files:
-
     file_path = os.path.join(current_directory, file)
-
     file_size = os.path.getsize(file_path)
-
     files_dict[file] = file_size
 
 print(files_dict)
-
-
-
-
